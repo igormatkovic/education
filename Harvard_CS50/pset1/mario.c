fd104ca1fdc36773d6530ba55862a878dadcaee5
Igor Matkovic <igor@cod3.me>
2015-10-06 13:55:45 -0400

    Update Mario with style50

--- Harvard_CS50/pset1/mario.c
+++ Harvard_CS50/pset1/mario.c
@@ -1,9 +1,11 @@
 /**
-  * Author: Igor Matkovic
-  *
-  * mario.c
-  * Problem Set 1 - Mario Piramid Problem
-  */
+ * mario.c
+ *
+ * Igor Matkovic
+ * igor@cod3.me
+ *
+ * Problem Set 1 - Mario Piramid Problem
+ */  
 
 #include <stdio.h>
 #include <cs50.h>
@@ -12,49 +14,56 @@
 void piramidLines(int, int);
 void printLine(string);
 
-
-int main(void) {
-
-    int height;
-
-    do {
-
-        if(height) {
-            printf("Height: ");
-        } else {
-            printf("Retry: ");
+int main(void)
+{
+    int height = 1;
+    do 
+    {
+        if (height) 
+        {
+            printf("Height:");
+        } 
+        else 
+        {
+            printf("Retry:");
         }
+		
+        height = GetInt();
+	} 
+	while (height < 1 || height > 23);
 
-		height = GetInt();
-
-	} while (height < 1 || height > 23);
-
-    for(int i = 0; i < height; i++) {
-       piramidLines(i, height);
+    for(int i = 0; i < height; i++) 
+    {
+        piramidLines(i, height);
     }
 }
-
-void piramidLines(int blocks, int total) {
+/*
+ * Draw piramid lines
+ */
+void piramidLines(int blocks, int total)
+{
 
     // Print out empty blocks first since the piramid is inverted
     int empty_blocks = total - blocks;
 
-    for(int i = 0; i <= empty_blocks; i++) {
+    for(int i = 0; i <= empty_blocks; i++) 
+    {
         printf(" ");
     }
 
-    for(int i = 0; i <= blocks; i++) {
+    for(int i = 0; i <= blocks; i++) 
+    {
         printf("#");
     }
-    //Allways finish with a extra Hash
-    printf("#");
-
-    printf("\n");
-
+    // Allways finish with a extra Hash
+    printf("#\n");
 }
 
-
-void printLine(string line) {
+/*
+ * Write a new Line in terminal
+ */
+void printLine(string line)
+{
 
     printf(" %s ", line);
 
