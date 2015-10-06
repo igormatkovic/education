326d2d2e6e55769beca288a2835c7a4efb1725a5
Igor Matkovic <igor@cod3.me>
2015-10-06 17:08:53 +0100

    Add greedy to the problem sets... TO DO.. read style guide

---
+++ Harvard_CS50/pset1/greedy.c
@@ -0,0 +1,104 @@
+/**
+ * greedy.c
+ *
+ * Igor Matkovic
+ * igor@cod3.me
+ *
+ * Problem Set 1 - Greedy cashier Problem
+ */
+
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int cointsInAmount(int, int);
+void printLine(string);
+
+
+int main(void) {
+    int quarter     = 25;
+    int dime        = 10;
+    int nickle      = 5;
+    int pennie      = 1;
+
+    int coins       = 0;
+    float change    = 0;
+
+
+    do {
+
+        printLine("O hai!  How much change is owed?");
+
+        change = round(100 * GetFloat());
+
+	} while (change <= 0);
+
+    
+
+    int quarters = cointsInAmount(change, quarter);
+
+    if(quarters > 0) {
+        change = change - (quarters * quarter);
+        coins += quarters;
+    }
+
+
+    int dimes = cointsInAmount(change, dime);
+
+    if(dimes > 0) {
+        change = change - (dimes * dime);
+        coins += dimes;
+    }
+
+
+
+    int nickles = cointsInAmount(change, nickle);
+
+    if(nickles > 0) {
+        change = change - (nickles * nickle);
+        coins += nickles;
+    }
+
+
+    int pennies = cointsInAmount(change, pennie);
+
+    if(pennies > 0) {
+        change = change - (nickles * pennie);
+        coins += pennies;
+    }
+
+
+    printf("%d\n", coins);
+
+}
+
+int cointsInAmount(int amount, int coin_value) {
+
+    int count = (amount / coin_value);
+
+    return count;
+
+}
+/*
+
+    do {
+
+        printLine("O hai!  How much change is owed?");
+
+        change = round(100 * GetFloat());
+
+	} while (change <= 0);
+
+
+
+    for(int i = 0; i < height; i++) {
+       piramidLines(i, height);
+    }
+*/
+
+
+void printLine(string line) {
+
+    printf(" %s ", line);
+
+}
